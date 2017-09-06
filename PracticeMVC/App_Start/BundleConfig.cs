94ac27e8a3a5875e5059692ba6c1eda809c2ecc2
Grey14 <greyhuang14@gmail.com>
2017-09-06 20:47:08 +0800

    Add project files.

---
+++ PracticeMVC/App_Start/BundleConfig.cs
@@ -0,0 +1,40 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace PracticeMVC
+{
+    public class BundleConfig
+    {
+        // 如需「搭配」的詳細資訊，請瀏覽 http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/dist/Scripts/jquery-{version}.js"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            //            "~/dist/Scripts/jquery.validate*"));
+
+            //// 使用開發版本的 Modernizr 進行開發並學習。然後，當您
+            //// 準備好實際執行時，請使用 http://modernizr.com 上的建置工具，只選擇您需要的測試。
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/dist/Scripts/modernizr-*"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/dist/Scripts/bootstrap.js",
+            //          "~/dist/Scripts/respond.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/Scripts").Include(
+                "~/dist/Scripts/*.js",
+                "~/dist/Scripts/jquery-{version}.js",
+                      "~/dist/Scripts/jquery.validate*",
+                      "~/dist/Scripts/modernizr-*",
+                      "~/dist/Scripts/bootstrap.js",
+                      "~/dist/Scripts/respond.js",
+                      "~/dist/Scripts/angular.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/dist/css/bootstrap.css",
+                      "~/dist/css/*.css"));
+        }
+    }
+}
---
+++ PracticeMVC/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using PracticeMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PracticeMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult GetStudentPropertyName()
+        {
+            string[] names = typeof(Student).GetProperties().Select(p => p.Name).ToArray();
+            return Json(names);
+        }
+
+        public ActionResult GetStudents()
+        {
+            //準備假資料
+            List<Student> data = new List<Student>();
+            data.Add(new Student { Name = "Wang Xiaoming", Height = 170, Weight = 72 });
+            data.Add(new Student { Name = "Zhang Xiaochun", Height = 180, Weight = 75 });
+            data.Add(new Student { Name = "Tian Fengsheng", Height = 175, Weight = 60 });
+            data.Add(new Student { Name = "Yang Ming Shan", Height = 165, Weight = 90 });
+            data.Add(new Student { Name = "Linghu Chong", Height = 172, Weight = 80 });
+            //計算BMI
+            foreach (var item in data)
+            {
+                var height = item.Height / 100;
+                item.BMI = Math.Round(item.Weight / (height * height),2);
+                if (item.BMI > 30)
+                    item.Memo = "too heavy!";
+                if (item.BMI < 20)
+                    item.Memo = "too thin!";
+            }
+
+            return Json(data);
+        }
+    }
+}
